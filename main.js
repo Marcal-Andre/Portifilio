db84347a33b92967d9276452b3f384f58d99c33b
Marcal-Andre <andremarcal.dev@gmail.com>
2024-09-14 11:56:30 -0300

    upando-detalhes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var typed= new typed(".text", {
-    String: ["Frontend Developer" , "Youtuber" , "Web Developer"],
-    typeSeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true,
+var typed= new Typed(".text", {
+    strings: ["Frontend Developer" , "Youtuber" , "Web Developer"],
+    typeSeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
 });
