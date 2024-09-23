06a069bc0750a18ea4452d1f89c1569a009d6e01
Marcal-Andre <andremarcal.dev@gmail.com>
2024-09-22 23:18:55 -0300

    upando..

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var typed= new typed(".text", {
-    Strings: ["Frontend Developer" , "Youtuber" , "Web Developer"],
-    typeSeed: 100,
+var typed= new Typed(".text", {
+    Strings: [ "Frontend Developer" , "Youtuber" , "Web Developer" ],
+    typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
-    loop: true
+    loop: true,
 });
